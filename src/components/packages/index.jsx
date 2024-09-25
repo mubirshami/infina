07da8e302fdde74c9c055941edf086d6dd0ffc66
mubirshami <mubirshami2000@gmail.com>
2024-09-25 23:31:10 +0500

    Styling

--- src/components/packages/index.jsx
+++ src/components/packages/index.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import "./index.css";
-import Package1Button from "../../assets/button_01.png";
-import Package2 from "../../assets/button_02.png";
-import Package3 from "../../assets/button_03.png";
 import Package1 from "../../assets/1.png";
-import Package2Button from "../../assets/2.png";
-import Package3Button from "../../assets/3.png";
+import Package2 from "../../assets/2.png";
+import Package3 from "../../assets/3.png";
 
 const Packages = () => {
   return (
@@ -28,25 +25,24 @@ const Packages = () => {
               </div>
             </div>
             <button>Buy Now</button>
-    
           </div>
         </div>
 
         <div className="package-1">
           <div className="package">
-            <h5>Standard</h5>
+            <h5>Professional</h5>
             <h5 className="percent-per-month">8% to 14% per month</h5>
 
-            <img src={Package1} alt="Package 1" className="package-image" />
-            <h5 className="price">200$ to 999$</h5>
+            <img src={Package2} alt="Package 1" className="package-image" />
+            <h5 className="price">1000$ to 4999$</h5>
             <div className="des">
               <div>
-                <div className="text">Basic</div>
-                <div className="text">200$ to 999$ </div>
-                <div className="text"> 8% to 14% per month</div>
                 <div className="text">
-                  total return 200% ( including capital )
+                  total return 250% (including capital)
                 </div>
+                <div className="text">Direct bonus 8%</div>
+                <div className="text">Binary . 7% of lesser team volume</div>
+                <div className="text">Binary Cap 2000$/Week</div>
               </div>
             </div>
             <button>Buy Now</button>
@@ -57,16 +53,16 @@ const Packages = () => {
           <div className="package">
             <h5>Premium</h5>
             <h5 className="percent-per-month">8% to 14% per month</h5>
-            <img src={Package1} alt="Package 1" className="package-image" />
-            <h5 className="price">200$ to 999$</h5>
+            <img src={Package3} alt="Package 1" className="package-image" />
+            <h5 className="price">5000 to 25000$</h5>
             <div className="des">
               <div>
-                <div className="text">Basic</div>
-                <div className="text">200$ to 999$ </div>
-                <div className="text"> 8% to 14% per month</div>
                 <div className="text">
-                  total return 200% ( including capital )
+                  total return 300% (including capital)
                 </div>
+                <div className="text">Direct bonus 8%</div>
+                <div className="text">Binary 8% of lesser team volume</div>
+                <div className="text">Binary Cap 7500$/Week</div>
               </div>
             </div>
             <button>Buy Now</button>
