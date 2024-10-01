e03b60f118f5e1595d7600397f17753ecd4e854c
mubirshami <mubirshami2000@gmail.com>
2024-10-01 15:35:36 +0500

    Refined the styling

--- src/components/forgot-password/index.jsx
+++ src/components/forgot-password/index.jsx
@@ -6,22 +6,22 @@ import { useNavigate } from "react-router-dom";
 const ForgotPassword = () => {
   const navigate = useNavigate();
   return (
-    <div className="login-container">
-      <div className="login-box">
+    <div className="forget-pass-container">
+      <div className="forget-pass-box">
         <form action="" className="form-styling">
-          <div className="login-header">
-            <img src={LoginImage} alt="Nenja Logo" className="login-logo" />
-            <h2 className="login-title">Forgot Password</h2>
+          <div className="forget-pass-header">
+            <img src={LoginImage} alt="Nenja Logo" className="forget-pass-logo" />
+            <h2 className="forget-pass-title">Forgot Password</h2>
           </div>
-          <div className="login-fields">
+          <div className="forget-pass-fields">
             <input
               type="text"
               placeholder="Username"
-              className="login-input"
+              className="forget-pass-input"
             />
           </div>
-          <div className="login-button">
-          <button type="submit" className="login-btn">FORGOT PASSWORD</button>
+          <div className="forget-pass-button">
+          <button type="submit" className="forget-pass-btn">FORGOT PASSWORD</button>
           </div>
           <div className="forgot-pass-signin-button">
             <p>Do you know the password already?</p>
