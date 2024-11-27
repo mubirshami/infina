ed7aff51e0a5f3ff0c6d34ab9a9f54e6ac62b5d0
mubirshami <mubirshami2000@gmail.com>
2024-11-27 14:51:04 +0500

    Added tailwind css

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import "./index.css";
 const Button = ({ text, onClick }) => {
   return (
-    <button className="button" onClick={onClick}>
-      {text} 
+    <button
+      className="bg-customBlue text-white text-center py-2 px-4 cursor-pointer m-1 border-1 font-extralight rounded-2xl"
+      onClick={onClick}
+    >
+      {text}
     </button>
   );
 };
